f5835a67482d210e21e2155d62e79b8df92b1096
v19187v045 <115050360+v19187v045@users.noreply.github.com>
2022-10-04 20:24:27 -0500

    Add files via upload

---
+++ hoy.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <?php
+    echo '<center>';
+    echo '<h1>';
+    echo "Hola soy PHP";
+    echo '</h1>';
+    echo '</center>';
+    echo "<h1>";
+    echo "<center>";
+    echo date("d-m-y");
+    echo "</center>";
+    echo "</h1>";
+    ?>
+</body>
+</html>
